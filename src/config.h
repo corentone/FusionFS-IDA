485bb2201ae092ac6aac5108392cfdc3fce7b6ea
peteratt <paltat@gmail.com>
2012-06-19 19:04:16 -0500

    Added the frontend skeleton

---
+++ src/config.h
@@ -0,0 +1,1 @@
+// Struct for the config file
---
+++ src/ida.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "idaAPI.h"
+
+int main(int argc, char *argv[]) {
+
+	char *mode = argv[1];
+	char *filename = argv[2]; // Maybe we'll have to change it to std::string
+	
+	// Create idaAPI object
+	idaAPI *dfs = new idaAPI();
+	
+	if (strcmp(mode, "insert") == 0) {
+		// Call insert API
+		dfs.insert(filename);
+	} else if (strcmp(mode, "get") == 0) {
+		// Call get API
+		dfs.get(filename);
+	} else if (strcmp(mode, "remove") == 0) {
+		// Call remove API
+		dfs.remove(filename);
+	} else {
+		// Call error
+		cout << "Usage: ida [insert/get/remove] [filename]";
+		return 1;
+	}
+	
+	return 0;
+}
